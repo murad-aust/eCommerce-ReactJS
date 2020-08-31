01b6ffb8b7bd8cf9e3d23449c9f5d9cb9e9b545f
murad-aust <murad.rony15@gmail.com>
2020-08-31 12:18:20 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header/Header';
+import Shop from './components/Shop/Shop';
+import Review from './components/Review/Review';
+import Manage from './components/Manage/Manage';
+import NotFound from './components/NotFound/NotFound';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
+import Product from './components/Product/Product';
+import ProductDetail from './components/ProductDetail/ProductDetail';
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <Router>
+            <Switch>
+              <Route path="/shop">
+              <Shop></Shop>
+              </Route>
+              <Route path="/review">
+                <Review></Review>
+              </Route>
+              <Route path="/manage">
+                <Manage></Manage>
+              </Route>
+              <Route exact path="/">
+              <Shop></Shop>
+              </Route>
+              <Route path="/product/:productkey">
+                <ProductDetail></ProductDetail>
+              </Route>
+              <Route path="*">
+                <NotFound></NotFound>
+              </Route>
+            </Switch>
+      </Router>
+
+        
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Product/Product.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import './Product.css';
+import { Link } from 'react-router-dom';
+
+const Product = (props) => {
+
+    const {img, name, seller, price, stock, key}= props.product;
+    return (
+        <div className= "product">
+            <div>
+                <img src={img} alt=""/>
+            </div>
+            <div>
+                <h3 className= "product-name">
+        
+                 <Link to={"/product/"+key}>{name}</Link></h3>
+                <br/>
+                <p> <small>by: {seller}</small></p>
+                <p>${price}</p>
+                <br/>
+                <p><small>Only {stock} left in Stock - Order soon</small></p>
+                {props.showAddToCart && <button className= "main-button" onClick= {() => props.handleAddProduct(props.product) }><FontAwesomeIcon icon={faShoppingCart} />Add to Cart</button>}
+            </div>
+          
+        </div>
+    );
+};
+
+export default Product;
